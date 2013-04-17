9e3ce6aafcc00c5c73ce2f0a7e08212afc8fcce4
Rayman <rayman747@hotmail.com>
2013-04-16 22:13:37 -0400

    add socket.io

--- app.js
+++ app.js
@@ -31,6 +31,15 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
+var server = http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
+});
+
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
 });
