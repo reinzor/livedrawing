ef3237f28cc24d7b17c4b214048e96c8926006ff
Rayman <rayman747@hotmail.com>
2013-04-22 22:08:44 -0400

    add qr.png route

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,32 @@
-
 /*
  * Routes
  */
 
+var QRCode = require('qrcode');
+
 exports.client = function(req, res){
-  res.render('client', { title: 'Client' });
+	res.render('client', { title: 'Client' });
 };
 
 exports.drawboard = function(req, res){
-  res.render('drawboard', { title: 'Drawboard' });
+	res.render('drawboard', { title: 'Drawboard' });
+};
+
+exports.qr = function(req, res) {
+	res.type('image/png');
+
+
+	QRCode.draw('i am a pony', function(error, canvas) {
+		if (error) {
+			console.log(err);
+			throw err;
+		}
+		canvas.toBuffer(function(err, buf){
+			if (err) {
+				console.log(err);
+				throw err;
+			}
+			res.send(buf);
+		});
+	});
 };
