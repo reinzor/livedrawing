668478119c62066fb9e1bc98fec91bd182bb4ae2
Rayman <rayman747@hotmail.com>
2013-04-22 22:44:58 -0400

    revert qr commit

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,12 @@
+
 /*
  * Routes
  */
 
-var QRCode = require('qrcode');
-
 exports.client = function(req, res){
-	res.render('client', { title: 'Client' });
+  res.render('client', { title: 'Client' });
 };
 
 exports.drawboard = function(req, res){
-	res.render('drawboard', { title: 'Drawboard' });
-};
-
-exports.qr = function(req, res) {
-	res.type('image/png');
-
-
-	QRCode.draw('i am a pony', function(error, canvas) {
-		if (error) {
-			console.log(err);
-			throw err;
-		}
-		canvas.toBuffer(function(err, buf){
-			if (err) {
-				console.log(err);
-				throw err;
-			}
-			res.send(buf);
-		});
-	});
+  res.render('drawboard', { title: 'Drawboard' });
 };
